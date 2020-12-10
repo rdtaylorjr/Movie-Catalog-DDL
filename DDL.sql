f7db4b16f5c1f13304a3800ee98ea2f2119a7f14
Russell Taylor <rdtaylorjr@protonmail.com>
2020-12-10 17:36:01 -0600

    Add files via upload

--- DDL.sql
+++ DDL.sql
@@ -8,13 +8,6 @@
  
  Overview
  In this exercise, we will practice our DDL skills by scripting a new database, tables, and relationships in a script file.
-
- This exercise is provided to give you practice applying the skills you have learned in this course, but you will not submit it to your instructor nor will it be graded.
-
- You should complete this exercise before continuing on with the course.
- If you have problems completing this exercise, review the course content up to this point and try again.
- Contact an instructor or TA if you have questions about this exercise or cannot resolve problems on your own.
- Remember to sync your code with your classwork repository after you have completed the exercise.
  
  Specification
  The database should keep track of movies we are interested in. Create a .sql script file to create a database named MovieCatalogue that contains the following tables:
@@ -52,6 +45,7 @@
  ActorID - Foreign key, Actor table, Required
  MovieID - Foreign key, Movie table, Required
  Role - Required, Extended character set, Length: 50
+ 
  */
 
 DROP DATABASE IF EXISTS MovieCatalogue;
@@ -60,12 +54,14 @@ CREATE DATABASE MovieCatalogue;
 
 USE MovieCatalogue;
 
-DROP TABLE IF EXISTS `MovieCatalogue`.`Genre`;
-CREATE TABLE `MovieCatalogue`.`Genre` (
-  `GenreID` INT NOT NULL AUTO_INCREMENT,
-  `GenreName` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`GenreID`));
-
+DROP TABLE IF EXISTS `MovieCatalogue`.`Actor`;
+CREATE TABLE `MovieCatalogue`.`Actor` (
+  `ActorID` INT NOT NULL AUTO_INCREMENT,
+  `FirstName` VARCHAR(30) NOT NULL,
+  `LastName` VARCHAR(30) NOT NULL,
+  `BirthDate` DATE NULL,
+  PRIMARY KEY (`ActorID`));
+  
 DROP TABLE IF EXISTS `MovieCatalogue`.`Director`;
 CREATE TABLE `MovieCatalogue`.`Director` (
   `DirectorID` INT NOT NULL AUTO_INCREMENT,
@@ -79,6 +75,12 @@ CREATE TABLE `MovieCatalogue`.`Rating` (
   `RatingID` INT NOT NULL AUTO_INCREMENT,
   `RatingName` VARCHAR(5) NOT NULL,
   PRIMARY KEY (`RatingID`));
+
+DROP TABLE IF EXISTS `MovieCatalogue`.`Genre`;
+CREATE TABLE `MovieCatalogue`.`Genre` (
+  `GenreID` INT NOT NULL AUTO_INCREMENT,
+  `GenreName` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`GenreID`));
 
 DROP TABLE IF EXISTS `MovieCatalogue`.`Movie`;
 CREATE TABLE `MovieCatalogue`.`Movie` (
@@ -98,14 +100,6 @@ CREATE TABLE `MovieCatalogue`.`Movie` (
   CONSTRAINT `fk_RatingID`
     FOREIGN KEY (`RatingID`)
     REFERENCES `MovieCatalogue`.`Rating` (`RatingID`));
-
-DROP TABLE IF EXISTS `MovieCatalogue`.`Actor`;
-CREATE TABLE `MovieCatalogue`.`Actor` (
-  `ActorID` INT NOT NULL AUTO_INCREMENT,
-  `FirstName` VARCHAR(30) NOT NULL,
-  `LastName` VARCHAR(30) NOT NULL,
-  `BirthDate` DATE NULL,
-  PRIMARY KEY (`ActorID`));
 
 DROP TABLE IF EXISTS `MovieCatalogue`.`CastMember`;
 CREATE TABLE `MovieCatalogue`.`CastMember` (
